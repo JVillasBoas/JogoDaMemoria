0fb3889bf9216f0235ce30ebc5e6a12d5e7b41df
Jesss <108489705+JVillasBoas@users.noreply.github.com>
2025-04-25 12:00:20 -0300

    Co-authored-by: Minikito <Artur544@users.noreply.github.com>

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
 
-export default function App() {
+const App = () => {
+  // const [count, setCount] = useState(0);
+  // const onPress = () => setCount(prevCount => prevCount + 1);
+  const [name, setName] = useState("")
+  const onPress = (param) => {setName(param);};
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.countContainer}>
+          <Text>nome: {name}</Text>
+        </View>
+
+        <View>
+          <TouchableOpacity style={styles.button} onPress={() => onPress("Zelda Image")}>
+            <Image
+              style={styles.img}
+              source={
+              require('./assets/zelda1.jpeg')
+              }
+            />
+          </TouchableOpacity>
+        </View>
+        
+        </SafeAreaView>
+    </SafeAreaProvider>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    paddingHorizontal: 10,
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+  },
+  countContainer: {
+    alignItems: 'center',
+    padding: 10,
+  },
+  img: {
+    width: 30,
+    height: 30,
   },
 });
+
+export default App;
